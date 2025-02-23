b7a86b4968e40f90ab01e20b0d4bd690ed1ee36e
Nethsarani   Sooriyaarachchi <nethsaranisooriyaarachchi89@gmail.com>
2025-02-23 19:54:28 +0530

    Update new1.py

--- new1.py
+++ new1.py
@@ -1,3 +1,6 @@
 num1=int(input("Enter First Number:"));
 num2=int(input("Enter Second number:"));
-Sum=num1+num2               
+Sum=num1+num2   
+Product=num1*num2
+print("Sum:",Sum)
+print("Product:",Product)
